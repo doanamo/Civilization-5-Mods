f86583c9342f9887b6f8bb6ceb391ef3645296b8
Peter Doan <doanpiotr@gmail.com>
2014-09-09 12:49:39 +0200

    Initial support for different languages.

--- Source/CvGameCoreDLL_Expansion2/Custom/QuietDiplomacy.cpp
+++ Source/CvGameCoreDLL_Expansion2/Custom/QuietDiplomacy.cpp
@@ -1,8 +1,17 @@
-#include "CvGameCoreDLLPCH.h"
+﻿#include "CvGameCoreDLLPCH.h"
 #include "QuietDiplomacy.h"
 
 #include "CvPlayer.h"
 #include "CvNotifications.h"
+
+namespace
+{
+    bool DoesTextKeyExist(const char* key)
+    {
+        std::string text = Localization::Lookup(key).toUTF8();
+        return text != key;
+    }
+}
 
 bool QuietDiplomacy::LeaderDiscussion(CvPlayer* human, CvPlayer* computer, const char* text)
 {
@@ -14,13 +23,55 @@ bool QuietDiplomacy::LeaderDiscussion(CvPlayer* human, CvPlayer* computer, const
     
     if(notifications)
     {
-        // Create strings.
-        Localization::String message = Localization::Lookup("TXT_KEY_QUIETDIPLOMACY_LEADERDISCUSSION_MESSAGE");
-        message << Localization::Lookup(computer->getNameKey());
-        message << text;
+        // Create localized strings.
+        // Hardcode some translation strings so DLL can be used alone without XML texts.
+        std::string language = Localization::GetCurrentLanguage().GetType();
 
-        Localization::String summary = Localization::Lookup("TXT_KEY_QUIETDIPLOMACY_LEADERDISCUSSION_SUMMARY");
-        summary << Localization::Lookup(computer->getNameKey());
+        std::string message;
+        std::string summary;
+
+        if(DoesTextKeyExist("TXT_KEY_QUIETDIPLOMACY_LEADERDISCUSSION_SUMMARY") && 
+            DoesTextKeyExist("TXT_KEY_QUIETDIPLOMACY_LEADERDISCUSSION_MESSAGE"))
+        {
+            // Fetch from the database.
+            Localization::String localeSummary = Localization::Lookup("TXT_KEY_QUIETDIPLOMACY_LEADERDISCUSSION_SUMMARY");
+            localeSummary << Localization::Lookup(computer->getNameKey());
+
+            Localization::String localeMessage = Localization::Lookup("TXT_KEY_QUIETDIPLOMACY_LEADERDISCUSSION_MESSAGE");
+            localeMessage << Localization::Lookup(computer->getNameKey());
+            localeMessage << text;
+
+            summary = localeSummary.toUTF8();
+            message = localeMessage.toUTF8();
+        }
+        else
+        {
+            if(language == "pl_PL")
+            {
+                // Polish
+                Localization::String localeLeader = Localization::Lookup(computer->getNameKey());
+
+                size_t localeLeaderBytes = 0;
+                const char* localeLeaderString = localeLeader.toUTF8(localeLeaderBytes, 2);
+
+                summary += "Wiadomo\xc5\x9b\xc4\x87 od ";
+                summary.append(localeLeaderString, localeLeaderBytes);
+
+                message += Localization::Lookup(computer->getNameKey()).toUTF8();
+                message += ": ";
+                message += text;
+            }
+            else
+            {
+                // English
+                summary += "Message from ";
+                summary += Localization::Lookup(computer->getNameKey()).toUTF8();
+
+                message += Localization::Lookup(computer->getNameKey()).toUTF8();
+                message += ": ";
+                message += text;
+            }
+        }
 
         // Get computer's capital.
         int x = -1;
@@ -34,7 +85,7 @@ bool QuietDiplomacy::LeaderDiscussion(CvPlayer* human, CvPlayer* computer, const
         }
 
         // Add a notification.
-        notifications->Add(NOTIFICATION_PEACE_ACTIVE_PLAYER, message.toUTF8(), summary.toUTF8(), x, y, computer->GetID());
+        notifications->Add(NOTIFICATION_PEACE_ACTIVE_PLAYER, message.c_str(), summary.c_str(), x, y, computer->GetID());
     }
 
     // Inform that we took care of it.
